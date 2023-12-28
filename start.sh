f693fcad40ef24163abf1f468ecbeaa9dab69ae8
dave2023 <dave2023@googlemail.com>
2023-12-28 14:42:08 +0800

    改为1min一个默认周期

--- start.sh
+++ start.sh
@@ -1,2 +1,25 @@
-/bin/pushgateway &
-/bin/prometheus "--config.file=/etc/prometheus/prometheus.yml" "--storage.tsdb.path=/prometheus" "--web.console.libraries=/usr/share/prometheus/console_libraries" "--web.console.templates=/usr/share/prometheus/consoles"
+#!/bin/sh
+if [ "$PUSHGATEWAY_USER" -a "$PUSHGATEWAY_PASS" ];then
+    PUSHGATEWAY_AUTH=$(echo "$PUSHGATEWAY_PASS" | htpasswd -niB -C 10 "$PUSHGATEWAY_USER"|sed 's#:#: #'| tr -d '\n')
+fi
+if [ "$PUSHGATEWAY_AUTH" ];then
+  cat <<EOF > /pushgateway.conf
+http_server_config:
+  http2: true
+  headers:
+    # Content-Security-Policy:
+    # X-Frame-Options:
+    # X-Content-Type-Options:
+    # X-XSS-Protection:
+    # Strict-Transport-Security:
+basic_auth_users:
+  $PUSHGATEWAY_AUTH
+EOF
+    /bin/pushgateway --web.config.file /pushgateway.conf &
+else
+    /bin/pushgateway &
+fi
+/bin/prometheus "--config.file=/etc/prometheus/prometheus.yml" \
+  "--storage.tsdb.path=/prometheus" \
+  "--web.console.libraries=/usr/share/prometheus/console_libraries" \
+  "--web.console.templates=/usr/share/prometheus/consoles"
